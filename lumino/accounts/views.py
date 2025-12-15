8010bbbf793a5fc4aeb6b8fcb7dae4eefc957796
Ruben Abreu Gonzalez <rubalba.rag@gmail.com>
2025-12-15 12:25:22 +0000

    feat: subject_list method started

--- lumino/accounts/views.py
+++ lumino/accounts/views.py
@@ -5,6 +5,7 @@ from django.urls import reverse
 from django.contrib import messages
 from django.utils.translation import gettext as _
 from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.decorators import login_required
 
 from .forms import UserSignupForm
 
@@ -25,9 +26,10 @@ def user_login(request):
         form = AuthenticationForm()
     return render(request, 'form.html', {'form': form, 'type':'login'})
 
+@login_required
 def user_logout(request):
     logout(request)
-    return redirect(request.GET.get('next', reverse(FALLBACK_REDIRECT)))
+    return redirect(reverse(FALLBACK_REDIRECT))
 
 def user_signup(request):
     if request.user.is_authenticated:
--- lumino/subjects/views.py
+++ lumino/subjects/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
 from .models import Subject
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def subject_list(request):
-    subjects = {}
-    return render(request, 'subjects.html', dict(subjects=subjects))
+    modules = {}
+    if request.user.profile.role == 'S':
+        modules = student_subject_list(request)
+    elif request.user.profile.role == 'T':
+        modules = teacher_subject_list(request)
+    return render(request, 'subjects.html', dict(modules=modules))
+
+def student_subject_list(request):
+    return request.user.enrollments.all()
+
+
+def teacher_subject_list(request):
+    return request.user.teaching.all()
