fd15712a618977d0f20810b39d39c69d90373e91
Ruben Abreu Gonzalez <rubalba.rag@gmail.com>
2025-12-29 12:14:39 +0000

    feat: comando de gestion get_subject_stats hecho

---
+++ lumino/subjects/management/commands/get_subject_stats.py
@@ -0,0 +1,18 @@
+from django.core.management.base import BaseCommand
+from subjects.models import Subject, Enrollment
+from django.db.models import Avg
+
+EMPTY_MARK = 0.00
+ROUND_NUM = 2
+
+class Command(BaseCommand):
+    help = 'Show the average grade for each module.'
+
+    def handle(self, *args, **kwargs):
+        for subject in Subject.objects.all():
+            if (enrollments := Enrollment.objects.filter(subject=subject).exclude(mark__isnull=True)):
+                avg_mark = enrollments.aggregate(avg_mark=Avg('mark'))['avg_mark']
+                self.stdout.write(f'{subject.code}: {round(avg_mark, ROUND_NUM)}')
+            else:
+                self.stdout.write(f'{subject.code}: {EMPTY_MARK}')
+                
