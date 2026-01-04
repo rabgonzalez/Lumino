dfb501a28c303790a9baac3fc20f763d526f64c9
Ruben Abreu Gonzalez <rubalba.rag@gmail.com>
2026-01-04 18:28:09 +0000

    feat: certificado funcional

--- lumino/subjects/tasks.py
+++ lumino/subjects/tasks.py
@@ -1,12 +1,21 @@
 from django_rq import job
 from django.core.mail import EmailMessage
+from weasyprint import HTML
+from django.conf import settings
+from pathlib import Path
+from django.template.loader import render_to_string
 
 @job
 def deliver_certificate(base_url, student):
+    output_dir = Path(settings.MEDIA_ROOT) / 'certificates'
+    output_path = f'{output_dir}/{student.username}_grade_certificate.pdf'
+    
+    html_string = render_to_string('certificate.html', {'student': student})
+    HTML(string=html_string, base_url=base_url).write_pdf(output_path)
     email = EmailMessage(
-        subject='Email test',
-        body='<h3>Hello there!</h3> <p>This is the email body</p>',
+        subject=f'{student.username} grades certificate',
         to=[student.email],
     )
     email.content_subtype = 'html'
+    email.attach_file(output_path)
     email.send()
