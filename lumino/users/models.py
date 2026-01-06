c19254e10a5f07eeea082ee46d7bc78b0de04b79
Ruben Abreu Gonzalez <rubalba.rag@gmail.com>
2026-01-06 11:33:46 +0000

    tests: test_core terminado

--- lumino/users/models.py
+++ lumino/users/models.py
@@ -10,7 +10,7 @@ class Profile(models.Model):
 
     user = models.OneToOneField(get_user_model(), related_name='profile', on_delete=models.CASCADE)
     role = models.CharField(choices=Role, max_length=1, default=Role.STUDENT)
-    avatar = models.ImageField(blank=True, upload_to='avatars/', default='avatars/noavatar.png')
+    avatar = models.ImageField(blank=True, upload_to='avatars', default='avatars/noavatar.png')
     bio = models.TextField(blank=True)
 
     def get_absolute_url(self):
