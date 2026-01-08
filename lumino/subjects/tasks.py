e9d225c7268d83ddf215843868aae6ffc7549efb
Ruben Abreu Gonzalez <rubalba.rag@gmail.com>
2026-01-08 11:50:35 +0000

    test: test del certificado solucionado

--- lumino/subjects/tasks.py
+++ lumino/subjects/tasks.py
@@ -1,14 +1,11 @@
 from django_rq import job
 from django.core.mail import EmailMessage
 from weasyprint import HTML
-from django.conf import settings
-from pathlib import Path
 from django.template.loader import render_to_string
 
 @job
 def deliver_certificate(base_url, student):
-    output_dir = Path(settings.MEDIA_ROOT) / 'certificates'
-    output_path = f'{output_dir}/{student.username}_grade_certificate.pdf'
+    output_path = f'media/certificates/{student.username}_grade_certificate.pdf'
     
     html_string = render_to_string('certificate.html', {'student': student})
     HTML(string=html_string, base_url=base_url).write_pdf(output_path)
